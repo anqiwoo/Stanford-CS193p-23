8f5bf4d296f1911fbf6ee97972cac50b8fedf6d4
Anqi Wu <anqiwu85@outlook.com>
2024-06-21 19:39:43 -0700

    Day 2: Lecture 1 half way

---
+++ assignment1/Memorize/Memorize/ContentView.swift
@@ -0,0 +1,33 @@
+//
+//  ContentView.swift
+//  Memorize
+//
+//  Created by Anqi Wu on 6/21/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .foregroundColor(Color.red)
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, CS139p!")
+        }
+        .padding()
+    }
+}
+
+
+
+
+
+
+
+
+
+#Preview {
+    ContentView()
+}
---
+++ assignment1/Memorize/Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Anqi Wu on 6/21/24.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
