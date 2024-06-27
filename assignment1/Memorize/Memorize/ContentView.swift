7bc5d594e48d9f60f2c3a46260a4190cc4c6aee6
Anqi Wu <anqiwu85@outlook.com>
2024-06-27 15:43:19 -0700

    Add tap and flip card logic

--- assignment1/Memorize/Memorize/ContentView.swift
+++ assignment1/Memorize/Memorize/ContentView.swift
@@ -12,7 +12,7 @@ struct ContentView: View {
         HStack {
             CardView(isFaceUp: true)
             CardView()
-            CardView(isFaceUp: true)
+            CardView()
             CardView()
         }
         .foregroundColor(.orange)
@@ -21,19 +21,21 @@ struct ContentView: View {
 }
 
 struct CardView: View {
-    var isFaceUp: Bool = false
+    @State var isFaceUp = false
     
     var body: some View {
         ZStack {
+            let base = RoundedRectangle(cornerRadius: 12)
             if isFaceUp {
-                RoundedRectangle(cornerRadius: 12)
-                    .foregroundColor(.white)
-                RoundedRectangle(cornerRadius: 12)
-                    .strokeBorder(lineWidth: 2)
+                base.foregroundColor(.white)
+                base.strokeBorder(lineWidth: 2)
                 Text("👻").font(.largeTitle)
             } else {
-                RoundedRectangle(cornerRadius: 12)
+                base
             }
+        }
+        .onTapGesture {
+            isFaceUp.toggle()
         }
     }
 }
